fc638fb0299bdc1b5f4ec157f5fd81917c06254d
lyp <1394665895@qq.com>
2020-09-11 11:35:21 +0800

    初始化提交

---
+++ src/com/company/Hello.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @program: gittest
+ * @description:
+ * @author: lyp
+ * @create: 2020-09-11 11:31:41
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
